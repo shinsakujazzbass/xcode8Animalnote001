fb8c66450a73772f031057a09a81196f6a6e5f06
shinsaku@sns.ne.jp <shinsaku@sns.ne.jp>
2017-05-09 19:02:10 +0900

    Initial Commit

---
+++ SwiftyJson Only01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyJson Only01
+//
+//  Created by Mac on 2017/05/09.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
